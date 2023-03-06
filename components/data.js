f6cad2079c194324b1064b0f1a5d8d076c3fc281
MarcosDMT <marcosgav80@gmail.com>
2023-03-06 22:21:25 +0300

    added dummy json for menus

---
+++ components/data.js
@@ -0,0 +1,37 @@
+export const data = [
+    {
+        image: '/images/space.jpg',
+        title: 'Reecent Launch',
+        desc: 'Crew-6 mission',
+        btn: 'rewatch',
+    },
+    {
+        image: '/images/space1.jpg',
+        title: 'Reecent Launch',
+        desc: 'Starlink Mission',
+        btn: 'rewatch',
+    },
+    {
+        image: '/images/space2.jpg',
+        title: 'Starship Update',
+        btn: 'learn more',
+    },
+    {
+        image: '/images/space3.jpg',
+        title: 'Starship selected by Nasa to support sustained Lunar Exploration',
+        desc: 'Crew-6 mission',
+        btn: 'learn more',
+    },
+]
+
+export const menu =[
+    'Falcon 9',
+    'Falcon Heavy',
+    'Dragon',
+    'Starship',
+    'Human Space flight',
+    'Rideshare',
+    'Starshield',
+    'StarLink',
+    'Shop',
+]
---
+++ pages/space.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Space = () => {
+  return (
+    <>
+    <section>
+      
+    </section>
+    </>
+  )
+}
+
+export default Space
