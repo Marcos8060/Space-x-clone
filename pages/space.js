d8bc8ac8248beb6eb1c4bd82eea2b67e11cb0304
MarcosDMT <marcosgav80@gmail.com>
2023-03-06 22:25:57 +0300

    mapped the json data

--- pages/space.js
+++ pages/space.js
@@ -1,10 +1,16 @@
+import { data } from '@/components/data'
 import React from 'react'
+
 
 const Space = () => {
   return (
     <>
     <section>
-      
+      {data.map((item,index) =>(
+        <div key={index}>
+          <img src={item.image} alt="" />
+        </div>
+      ))}
     </section>
     </>
   )
